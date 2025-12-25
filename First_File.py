1ea2a543b6be61aedc6b0debb0d8d2b5b04c2aa5
Sagar Marthandan <sagar.marthandan@gmail.com>
2025-12-25 13:01:43 +0530

    commit after dev_branch merge

--- First_File.py
+++ First_File.py
@@ -3,3 +3,8 @@ print("Hello, World!")
 
 def add_numbers(a, b):
     return a + b
+
+
+# This is a simple function to demonstrate functionality
+result = add_numbers(5, 7)
+print(f"The sum of 5 and 7 is: {result}")
